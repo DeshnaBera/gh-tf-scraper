40ded71c0b6b3864226d9a5f01a46c9e341ecdbe
DeshnaBera <beradeshna@gmail.com>
2024-07-02 11:49:03 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+ 
+resource "aws_lambda_function" "example" {
+  function_name = "hello"
+ 
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = var.s3_bucket
+  s3_key    = var.s3_key
+ 
+  handler = var.lambda_handler
+  runtime = "python3.10"
+ 
+  role = aws_iam_role.iam_for_lambda.arn
+}
+ 
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+ 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+s3_bucket = "lambda-upload0620"
+s3_key    = "lambda_hello.zip"
+lambda_handler = "lambda_hello.lambda_handler" 
